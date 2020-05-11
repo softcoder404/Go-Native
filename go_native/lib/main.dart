b8d0aa4d4e5db3e8d830a93d2b8a2ea511a592d9
softcoder404 <daseeker9@gmail.com>
2020-05-11 17:10:26 +0100

    new features added to the main screen

--- go_native/lib/main.dart
+++ go_native/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+
 
 import 'package:flutter/services.dart';
 import 'package:speech_to_text_plugins/speech_to_text_plugins.dart';
@@ -14,7 +14,10 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   List<String> _suggestions = [];
   String _tapText = "Tap to start speaking";
-  String _msg = "";
+  String senderMsg = "";
+  String senderName = "Sophia";
+  String receiverMsg = "";
+  String receiverName = "Bolaji";
   final _textController = TextEditingController();
   @override
   void initState() {
@@ -38,59 +41,128 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
+            Expanded(
+              child: Container(
+                child: ListView(
                   children: [
-                    GestureDetector(
-                      onTap: () {
-                        SpeechToTextPlugins speechToTextPlugins =
-                            SpeechToTextPlugins();
-                        speechToTextPlugins.activate().then((onValue) {
-                          print(onValue);
-                          onValue
-                              ? setState(() {
-                                  _tapText = "Start speaking....";
-                                })
-                              : setState(() {
-                                  _tapText = "Tap again to start speaking";
+                    SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                SpeechToTextPlugins speechToTextPlugins =
+                                    SpeechToTextPlugins();
+                                speechToTextPlugins.activate().then((onValue) {
+                                  print(onValue);
+                                  onValue
+                                      ? setState(() {
+                                          _tapText = "Start speaking....";
+                                        })
+                                      : setState(() {
+                                          _tapText =
+                                              "Tap again to start speaking";
+                                        });
                                 });
-                        });
-                        speechToTextPlugins.listen().then((onValue) {
-                          print(onValue);
-                          setState(() {
-                            _tapText = "Tap again to start speaking";
-                            _suggestions.clear();
-                            onValue.forEach((text) => _suggestions.add(text));
-                          });
-                        });
-                      },
+                                speechToTextPlugins.listen().then((onValue) {
+                                  print(onValue);
+                                  setState(() {
+                                    _tapText = "Tap again to start speaking";
+                                    _suggestions.clear();
+                                    onValue.forEach(
+                                        (text) => _suggestions.add(text));
+                                  });
+                                });
+                              },
+                              child: Container(
+                                height: 100,
+                                width: 100,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(60.0),
+                                  image: DecorationImage(
+                                    image:
+                                        AssetImage("assets/images/speak.png"),
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Text(_tapText),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    if (_suggestions.length > 0) suggestionResult(),
+                    Opacity(
+                      opacity: senderMsg.isEmpty ? 0 : 1,
                       child: Container(
-                        height: 100,
-                        width: 100,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(60.0),
-                          image: DecorationImage(
-                            image: AssetImage("assets/images/speak.png"),
-                            fit: BoxFit.fill,
-                          ),
+                        margin: EdgeInsets.symmetric(
+                            horizontal: 15.0, vertical: 5.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Container(
+                              alignment: Alignment.centerRight,
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(senderName),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.all(15.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.red[200],
+                                    borderRadius: BorderRadius.circular(25.0),
+                                  ),
+                                  child: Text(
+                                    senderMsg,
+                                    style: TextStyle(
+                                        fontSize: 22.0, color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                    Text(_tapText),
+                    Opacity(
+                      opacity: receiverMsg.isEmpty ? 0.0 : 1.0,
+                      child: Container(
+                        margin: EdgeInsets.symmetric(
+                            horizontal: 15.0, vertical: 20.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(receiverName),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(15.0),
+                              decoration: BoxDecoration(
+                                color: Colors.green[200],
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                              child: Text(receiverMsg,
+                                  style: TextStyle(
+                                      fontSize: 22.0, color: Colors.white)),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
                   ],
                 ),
-              ],
-            ),
-            SizedBox(height: 15),
-            if (_suggestions.length > 0) suggestionResult(),
-            Expanded(
-              child: Container(),
+              ),
             ),
             Container(
               width: double.infinity,
@@ -104,12 +176,14 @@ class _MyAppState extends State<MyApp> {
                     TextStyle(color: Colors.white, fontSize: 20.0, height: 1.2),
                 cursorColor: Colors.white,
                 keyboardType: TextInputType.multiline,
+                onChanged: (val) => _textController.text = val,
                 maxLines: null,
                 decoration: InputDecoration(
                     suffixIcon: IconButton(
                         onPressed: () {
                           setState(() {
-                            _msg = _textController.text;
+                            senderMsg = _textController.text;
+                            _textController.text = "";
                           });
                         },
                         icon: Icon(Icons.send, color: Colors.white)),
@@ -137,14 +211,21 @@ class _MyAppState extends State<MyApp> {
           direction: Axis.horizontal,
           children: [
             ..._suggestions
-                .map((text) => Container(
-                      decoration: BoxDecoration(
-                        color: Colors.red[100],
-                        borderRadius: BorderRadius.circular(12.0),
+                .map((text) => GestureDetector(
+                      onTap: () => setState(() {
+                        senderMsg = text;
+                        _suggestions.clear();
+                        print(senderMsg);
+                      }),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.red[100],
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        padding: const EdgeInsets.all(10.0),
+                        margin: EdgeInsets.all(10),
+                        child: Text(text),
                       ),
-                      padding: const EdgeInsets.all(10.0),
-                      margin: EdgeInsets.all(10),
-                      child: Text(text),
                     ))
                 .toList(),
           ],
